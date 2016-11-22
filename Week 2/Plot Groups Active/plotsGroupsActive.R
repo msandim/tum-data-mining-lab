7d9d099d2ad904410bc3cee24ca3e480f53c6e62
msandim <migsandim@hotmail.com>
2016-11-22 04:18:58 +0100

    Plots finished

--- Week 2/Plot Groups Active/plotsGroupsActive.R
+++ Week 2/Plot Groups Active/plotsGroupsActive.R
@@ -9,6 +9,8 @@ data_backup <- data
 data <- data[data$gname != "Unknown" & data$gname != "Other" & data$gname != "Unaffiliated Individual(s)",]
 data[data$imonth == 0, "imonth"] <- 1
 data[data$iday == 0, "iday"] <- 1
+data <- data[data$doubtterr == 1,]
+data <- data[!is.na(data$region_txt),]
 data$date <- as.Date(paste0(data$iyear, "/", data$imonth, "/", data$iday))
 data <- data %>% select(eventid, date, everything())
 
@@ -19,14 +21,16 @@ data_plot_region <- data %>%
   summarise(start = first(date), end = last(date), number_incidents = n()) %>%
   arrange(region_txt, desc(number_incidents)) %>%
   mutate(gname = paste0(gname, " | ", sprintf("%04d", number_incidents))) %>%
+  mutate(Duration = end - start) %>%
   rename(Region = region_txt, Group = gname, Start = start, End = end, Incidents = number_incidents)
-  
 
 data_plot_total <- data %>%
   group_by(gname) %>%
   arrange(date) %>%
   summarise(start = first(date), end = last(date), number_incidents = n()) %>%
   arrange(desc(number_incidents)) %>%
+  mutate(gname = paste0(gname, " | ", sprintf("%04d", number_incidents))) %>%
+  mutate(Duration = end - start) %>%
   rename(Group = gname, Start = start, End = end, Incidents = number_incidents)
 
 write.csv(data_plot_region, "data_by_region.csv", row.names = FALSE, fileEncoding = "UTF-8")
